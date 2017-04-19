a02290885c20d0fc32535023e2c1436c100a52c8
andremororo <andrefmororo@gmail.com>
2017-04-18 21:58:34 -0300

    Inserido 1º Commit

---
+++ AWSManager/src/main/java/com/amazonaws/samples/RunExample.java
@@ -0,0 +1,28 @@
+package com.amazonaws.samples;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+
+public class RunExample {
+
+	public static void main(String[] args) throws Exception {
+		
+		AWSCredentials credentials = new ProfileCredentialsProvider("default").getCredentials();
+        AmazonEC2 ec2 = new AmazonEC2Client(credentials);
+        
+        ec2.setEndpoint("ec2.us-east-1.amazonaws.com");
+        
+    	RunInstancesRequest runInstancesRequest = new RunInstancesRequest();
+    		        	
+    	runInstancesRequest.withImageId("ami-cecdd7d9")
+	                     .withInstanceType("t2.micro")
+	                     .withMinCount(1)
+	                     .withMaxCount(1)
+	                     .withKeyName("uniforTests")
+	                     .withSecurityGroups("default");
+	  
+    	ec2.runInstances(runInstancesRequest);
+    }
+}
